65ac34a27035e6f3b663424c99fabe44d5e4ae23
Muhaimin <mislam@wpi.edu>
2018-03-22 11:29:46 -0400

    Can Demo draft code 1

---
+++ can_demo.py
@@ -0,0 +1,24 @@
+import serial
+import time
+
+ser  = serial.Serial('/dev/ttyACM0', 9600)
+
+print("CAN bus server interface demo")
+
+while 1:
+    ser.write(b'i')
+    time.sleep(0.5)
+    print(ser.readline())
+    time.sleep(1)
+    ser.write(b'm')
+    time.sleep(0.5)
+    print(ser.readline())
+    time.sleep(1)
+    ser.write(b'k')
+    time.sleep(0.5)
+    print(ser.readline())
+    time.sleep(1)
+    ser.write(b'c')
+    time.sleep(0.5)
+    print(ser.readline())
+    time.sleep(1)
